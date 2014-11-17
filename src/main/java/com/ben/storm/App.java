716337b6fde1622f50de4899a559575720ef5114
Ben Wang <ben.wxc@gmail.com>
2014-11-16 19:37:19 -0500

    Inital commit

---
+++ src/main/java/com/ben/storm/App.java
@@ -0,0 +1,13 @@
+package com.ben.storm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
